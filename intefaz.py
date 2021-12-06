0b2909954fc556dd627c4d2bcd33d7435475f3b7
mrtopo1840 <juanfelipecortescastrillon@gmail.com>
2021-12-05 22:12:52 -0500

    Se modifican algunos nombres de archivos y carpetas, se modifica pygame para que se pueda ejecutar como funcion, se agrega conexion para que se pueda ejecutar pygame desde la interfaz, tambien se mueve el archivo 'interza.py' a la raiz por que desde ahi se va a ejecutar todo el programa

---
+++ intefaz.py
@@ -0,0 +1,32 @@
+import sys
+import numpy as np
+
+from interfaz.interfazAutomata import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+
+class Ventana(QWidget):
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.aleatorio.clicked.connect(self.Aleatorio)
+        self.ui.ejecutar.clicked.connect(self.Ejecutar)
+
+    def Aleatorio(self):
+        x = np.random.randint(255)
+        self.ui.automata_text.setText(str(x))
+
+    def Ejecutar(self):
+        if self.ui.automata_text.text() != None:
+            from automataPygame.automata2 import ejecutar_pygame
+            ejecutar_pygame(int(self.ui.automata_text.text()))
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    window = Ventana()
+    window.show()
+    sys.exit(app.exec_())
